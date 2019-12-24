be49f3e6761fc58c0f2cf79504d4a631130a1fe3
martin <mvarmuza@gmail.com>
2019-12-24 17:55:35 +0100

    style subscribe

--- components/Subscribe.jsx
+++ components/Subscribe.jsx
@@ -1,7 +1,48 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 const url = 'https://gmail.us4.list-manage.com/subscribe/post?u=0a8876c6f4f96cee684d03c89&amp;id=398d723510';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Button = styled.button`
+    margin: 5px;
+    background-color: black;
+    border: 5px solid yellow;
+    color: white;
+    marginTop: 5px;
+    width: 210px;
+    height: 40px;
+    cursor: pointer;
+    font-weight: bold;
+
+    &:hover {
+        background-color: yellow;
+        color: black;
+    }
+`;
+
+const Input = styled.input`
+    margin: 5px;
+    width: 200px;
+    height: 30px;
+    color: white;
+    border-radius: 2px;
+    border: yellow solid 5px;
+    background-color: black;
+    text-align: center;
+    transition: 0.3s all;
+    font-weight: bold;
+
+    &:focus {
+        outline: none;
+    }
+`;
 
 const Subscribe = () => {
   const [email, setEmail] = useState('');
@@ -11,15 +52,21 @@ const Subscribe = () => {
     setIsSending(true);
     await subscribe({ EMAIL: email });
     setIsSending(false);
+    setEmail('');
   };
 
   return (
     <MailchimpSubscribe
       url={url}
       render={({ subscribe, status, message }) => (
-        <div>
-          <input type="text" placeholder="Email" onChange={(event) => setEmail(event.target.value)} disabled={isSending} />
-          <button type="submit" onClick={() => send(subscribe)} disabled={isSending}>Odeslat</button>
+        <Wrapper>
+          <Input
+            type="text"
+            placeholder="Email"
+            onChange={(event) => setEmail(event.target.value)}
+            disabled={isSending}
+          />
+          <Button type="submit" onClick={() => send(subscribe)} disabled={isSending}>Odeslat</Button>
           {status === 'sending' && <div>odesílá se to...</div>}
           {status === 'error' && (
             <div>
@@ -28,10 +75,16 @@ neprošlo to:
             </div>
           )}
           {status === 'success' && <div>hotovo!</div>}
-        </div>
+        </Wrapper>
       )}
     />
   );
 };
 
 export default Subscribe;
+
+// .field
+//   .field:hover {
+//     background-color: rgba(255, 255, 255, 0.45);
+//     box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.05);
+//   }
--- public/texts/zapojit-se.md
+++ public/texts/zapojit-se.md
@@ -1,3 +1,5 @@
 # Zapojit se
 
-jak na to
+V první fázi chceme dát dohromady seznam lidí, které to zajímá. Ať už jste zvědavý
+jak celá věc dopadla nebo máte chuť se nějak zapojit, nechte nám, prosím, svůj email. 
+Slibujeme, že ho neprodáme na darknetu. 
